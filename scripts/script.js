ea0feaa806a3deebbb1b82116f75e38279b1de70
enver-isliamov <enverphoto@gmail.com>
2024-02-18 17:39:25 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,10 @@
+/*
+design by Voicu Apostol.
+design: https://dribbble.com/shots/3533847-Mini-Music-Player
+I can't find any open music api or mp3 api so i have to download all musics as mp3 file.
+You can fork on github: https://github.com/muhammederdem/mini-player
+*/
+
 new Vue({
   el: "#app",
   data() {
@@ -10,75 +17,75 @@ new Vue({
       isTimerPlaying: false,
       tracks: [
         {
-          name: "Mekanın Sahibi",
-          artist: "Norm Ender",
+          name: "by Enver Isliamov",
+          artist: "Canli Radio Online",
           cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/1.jpg",
-          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/1.mp3",
-          url: "https://www.youtube.com/watch?v=z3wAjJXbYzA",
+          source: "https://c28.radioboss.fm:18047/stream",
+          url: "https://www.youtube.com/@CanlRadio1",
           favorited: false
         },
         {
           name: "Everybody Knows",
           artist: "Leonard Cohen",
           cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/2.jpg",
-          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/2.mp3",
-          url: "https://www.youtube.com/watch?v=Lin-a2lTelg",
+          source: "https://c28.radioboss.fm:18047/stream",
+          url: "https://www.youtube.com/@CanlRadio1",
           favorited: true
         },
         {
           name: "Extreme Ways",
           artist: "Moby",
           cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/3.jpg",
-          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/3.mp3",
-          url: "https://www.youtube.com/watch?v=ICjyAe9S54c",
+          source: "https://c28.radioboss.fm:18047/stream",
+          url: "https://www.youtube.com/@CanlRadio1",
           favorited: false
         },
         {
           name: "Butterflies",
           artist: "Sia",
           cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/4.jpg",
-          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/4.mp3",
-          url: "https://www.youtube.com/watch?v=kYgGwWYOd9Y",
+          source: "https://c28.radioboss.fm:18047/stream",
+          url: "https://www.youtube.com/@CanlRadio1",
           favorited: false
         },
         {
           name: "The Final Victory",
           artist: "Haggard",
           cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/5.jpg",
-          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/5.mp3",
-          url: "https://www.youtube.com/watch?v=0WlpALnQdN8",
+          source: "https://c28.radioboss.fm:18047/stream",
+          url: "https://www.youtube.com/@CanlRadio1",
           favorited: true
         },
         {
           name: "Genius ft. Sia, Diplo, Labrinth",
           artist: "LSD",
           cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/6.jpg",
-          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/6.mp3",
-          url: "https://www.youtube.com/watch?v=HhoATZ1Imtw",
+          source: "https://c28.radioboss.fm:18047/stream",
+          url: "https://www.youtube.com/@CanlRadio1",
           favorited: false
         },
         {
           name: "The Comeback Kid",
           artist: "Lindi Ortega",
           cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/7.jpg",
-          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/7.mp3",
-          url: "https://www.youtube.com/watch?v=me6aoX0wCV8",
+          source: "https://c28.radioboss.fm:18047/stream",
+          url: "https://www.youtube.com/@CanlRadio1",
           favorited: true
         },
         {
           name: "Overdose",
           artist: "Grandson",
           cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/8.jpg",
-          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/8.mp3",
-          url: "https://www.youtube.com/watch?v=00-Rl3Jlx-o",
+          source: "https://c28.radioboss.fm:18047/stream",
+          url: "https://www.youtube.com/@CanlRadio1",
           favorited: false
         },
         {
           name: "Rag'n'Bone Man",
           artist: "Human",
           cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/9.jpg",
-          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/9.mp3",
-          url: "https://www.youtube.com/watch?v=L3wKzyIN1yk",
+          source: "https://c28.radioboss.fm:18047/stream",
+          url: "https://www.youtube.com/@CanlRadio1",
           favorited: false
         }
       ],
